a273b554dfe5a737a8780e03060d81a7247c0a9c
bartek8b <163062773+bartek8b@users.noreply.github.com>
2025-11-05 21:11:06 +0100

    Set functioin binarySearchTree(array)

--- bst.js
+++ bst.js
@@ -11,3 +11,22 @@ class Tree {
     this.root = null;
   }
 }
+
+function binarySearchTree(array) {
+  // Sort copy of input, do not mutate it
+  const sorted = [...array].sort((a, b) => a - b);
+
+  function buildTree(array, start = 0, end = array.length - 1) {
+    if (start > end) return null;
+
+    const mid = start + Math.floor((end - start) / 2);
+    const root = new Node(array[mid]);
+
+    root.left = buildTree(array, start, mid - 1);
+    root.right = buildTree(array, mid + 1, end);
+
+    return root;
+  }
+
+  return buildTree(sorted);
+}
